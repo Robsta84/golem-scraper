5f3a20d6d15c8a6ff34648bcff68d6a8ba6aca53
Robert Schwarze <robert@weloveapps.de>
2017-08-01 23:13:12 +0200

    added if __name__ == '__main__'

--- scraper.py
+++ scraper.py
@@ -39,23 +39,24 @@ def get_all_href_tags(list_of_header_elements, list_elements):
 
     return article_elements
     
+if __name__ == '__main__':
 
-if page.status_code == 200:
-    tree = page.content
-    soup = BeautifulSoup(tree, 'lxml')
-    section = soup.find('section')        
-    articles_list = soup.find_all('ol', "list-articles")       
-    list_elements = get_list_elements(articles_list)        
-    header_elements = get_header_elements(list_elements)
-    articles = get_all_href_tags(header_elements, list_elements)       
-    print len(articles)
-    for article in articles:
-        print "headline:     %s \n" % article.headline
-        print "short text:   %s \n" % article.short_text
-        print "link:         %s \n" % article.link 
-        print "\n"        
-else:
-    print ("Nothing to see here")
+    if page.status_code == 200:
+        tree = page.content
+        soup = BeautifulSoup(tree, 'lxml')
+        section = soup.find('section')        
+        articles_list = soup.find_all('ol', "list-articles")       
+        list_elements = get_list_elements(articles_list)        
+        header_elements = get_header_elements(list_elements)
+        articles = get_all_href_tags(header_elements, list_elements)       
+        print len(articles)
+        for article in articles:
+            print "headline:     %s \n" % article.headline
+            print "short text:   %s \n" % article.short_text
+            print "link:         %s \n" % article.link 
+            print "\n"        
+    else:
+        print ("Nothing to see here")
     
 
 
